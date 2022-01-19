1b7941ef344ff6149ebf4a15b4e50af575ea89e1
AnniesTech <anita.rocha0505@gmail.com>
2022-01-19 14:19:19 -0500

    tried point 8 using du's way but with for cycles

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""Class Square definition"""
+
+
+class Square:
+    """Class Square that defines a square:
+        - Private instance attribute: size
+        - Instantiation with optional size: def __init__(self, size=0)"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializer object"""
+        self.size = size
+        self.position = position
+
+    def area(self):
+        """Public instance method:
+            - Return: the current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Property of size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """Prints a square"""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for j in range(self.size):
+                print(' ' * self.position[0], end="")
+                print('#' * self.size)
+
+    @property
+    def position(self):
+        """Property of position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter of position"""
+        if not isinstance(value, tuple) or len(value) != 2 \
+           or not isinstance(value[0], int)\
+           or not isinstance(value[1], int) \
+           or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def __str__(self):
+        """Prints a square using my_square"""
+        result = ""
+        if self.size == 0:
+            return result
+        else:
+            for i in range(self.position[1]):
+                result = result + "\n"
+            for j in range(self.size):
+                result = result + " " * self.position[0]
+                result = result + "#" * self.size
+                if j != (self.size - 1):
+                    result = result + "\n"
+        return result
