1dc30658349261c68cb81ae6b1f67fa8b5c1f775
AnniesTech <anita.rocha0505@gmail.com>
2022-01-24 16:19:54 -0500

    3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" This is 3-say_my_name that prints My name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints the first and last name:
+        first_name: first argument
+        last_name: second argument
+        Return:
+            prints the name in the format given
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
