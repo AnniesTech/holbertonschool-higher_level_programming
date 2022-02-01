b4a830cfb7f38bf9fdd7f8316f38f7464df0a393
AnniesTech <anita.rocha0505@gmail.com>
2022-01-31 19:20:29 -0500

    readme and 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Definition of function lookup"""
+
+
+def lookup(obj):
+    """Functionthat returns the list of available attributes
+    and methods of an object"""
+    return dir(obj)
