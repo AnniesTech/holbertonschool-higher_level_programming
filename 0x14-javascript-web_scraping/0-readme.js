2f11e8c0890fd6f05107ca040cbfd23fbcb3974d
AnniesTech <anita.rocha0505@gmail.com>
2022-04-11 15:36:04 -0500

    fixed 0 and 1 working

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const fl = require('fl');
-fl.readFile(process.argv[2], 'utf-8', function (err, content) {
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', function (err, content) {
   if (content === undefined) {
     console.log(err);
   } else {
