b94b84f306f6df1188b067ca67685955460dfbdd
AnniesTech <anita.rocha0505@gmail.com>
2022-04-12 08:42:26 -0500

    not working with previous ditribution

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const fs = require('fs');
 const url = process.argv[2];
+
+const fs = require('fs');
 const file = process.argv[3];
+
 request(url, (err, response, body) => {
   if (err) {
     console.log(err);
