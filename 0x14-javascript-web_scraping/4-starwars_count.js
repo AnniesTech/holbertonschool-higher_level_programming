63fa18d3347c400a1f14de96108b25b41adb1af1
AnniesTech <anita.rocha0505@gmail.com>
2022-04-12 08:24:59 -0500

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-let cont = 0;
 request(url, (err, reponse, body) => {
   if (err) {
     console.log(err);
   } else {
+    let cont = 0;
     const respo = JSON.parse(body);
-    const results = respo.results;
-    for (let i = 0; i < results.length; i++) {
-      const characters = results[i].characters;
+    const result = respo.results;
+    for (let i = 0; i < result.length; i++) {
+      const characters = result[i].characters;
       for (let j = 0; j < characters.length; j++) {
         if (characters[j].search('18') > 0) {
           cont++;
