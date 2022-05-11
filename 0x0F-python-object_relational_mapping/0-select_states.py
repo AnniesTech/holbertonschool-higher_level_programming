bbf24dcfa9ccfc786ba410ae54f62cf3e72f5380
AnniesTech <anita.rocha0505@gmail.com>
2022-05-11 18:30:05 -0500

    task 0, proved on terminal

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,12 +12,12 @@ if __name__ == "__main__":
     argv[2]: password
     argv[3] database name.
     Results must be sorted in ascending order by states.id."""
-    temp = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
-    cursor = temp.cursor()
+    cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
     cursor.close()
-    temp.close()
+    db.close()
