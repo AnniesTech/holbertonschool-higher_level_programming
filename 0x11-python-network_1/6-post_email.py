387a1f29f85ea49cccb0d8b97aff5139cd15945b
AnniesTech <anita.rocha0505@gmail.com>
2022-05-19 19:28:30 -0500

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,8 +5,8 @@ sends a POST request to the passed URL with the email as a parameter,
 and finally displays the body of the response.
 """
 if __name__ == '__main__':
-    import requests
+    from requests import post
     from sys import argv
     payload = {'email': argv[2]}
-    r = requests.post(argv[1], data=payload)
+    r = post(argv[1], data=payload)
     print(r.text)
